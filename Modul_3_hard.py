66fb5ec418c48a1ef0e235e1c5370640fd49fc35
paul-b-b64 <belousko@inbox.ru>
2024-07-04 16:57:34 +0300

    Hard

---
+++ Modul_3_hard.py
@@ -0,0 +1,19 @@
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     proizv = factorial(n=n - 1)
+#     return n * proizv
+#
+#
+# print(factorial(5))
+
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+print(len(data_structure))
+print(len(data_structure[0]))
